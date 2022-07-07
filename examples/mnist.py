2d3b61b9b8f3218b91621a5abe2f02dbfeb41f6c
Luka Cehovin Zajc <luka.cehovin@gmail.com>
2022-07-07 17:55:34 +0200

    Random patch and MNIST examples

---
+++ examples/mnist.py
@@ -0,0 +1,53 @@
+import os
+import struct
+from itertools import count
+import cv2
+
+import numpy as np
+
+from pixelpipes.graph import RandomSeed, Constant
+from pixelpipes.list import RandomElement
+from pixelpipes.utilities import pipeline
+
+@pipeline()
+def mnist(images_file, labels_file):
+    images = []
+    labels = []
+    with open(images_file, mode="rb") as h:
+        h.read(4) # Magic number
+        count, height, width = struct.unpack(">III", h.read(12))
+        for _ in range(count):
+            images.append(np.frombuffer(h.read(width * height), dtype=np.uint8).reshape(height, width))
+
+    with open(labels_file, mode="rb") as h:
+        h.read(4) # Magic number
+        count, = struct.unpack(">i", h.read(4))
+        for _ in range(count):
+            labels.append(struct.unpack("B", h.read(1))[0])
+
+        assert len(images) == len(labels)
+
+    # Pipeline starts here
+
+    l = Constant(labels)
+    i = Constant(images)
+    s = RandomSeed() # Both label and image should be sampled the same way, we are binding the same random seed
+
+    return RandomElement(i, seed=s), RandomElement(l, seed=s)
+
+# Download original train or test files from http://yann.lecun.com/exdb/mnist/
+# unzip them and point the paths below to the final files
+
+root = os.path.dirname(__file__)
+images_file = os.path.join(root, "train-images.idx3-ubyte")
+labels_file = os.path.join(root, "train-labels.idx1-ubyte")
+
+p = mnist(images_file, labels_file)
+
+for i in count(1):
+    image, label = p.run(i)
+    print(label)
+    cv2.imshow("Patch", image)
+    if cv2.waitKey() != 32:
+        break
+
---
+++ examples/patches.py
@@ -0,0 +1,25 @@
+import os
+from itertools import count
+
+import cv2
+
+from pixelpipes.resource.loading import ImageDirectory
+from pixelpipes.resource.list import RandomResource
+from pixelpipes.image.geometry import ViewImage
+from pixelpipes.resource.patch import RandomPatchView
+from pixelpipes.utilities import pipeline
+
+@pipeline()
+def stream():
+    images = ImageDirectory(os.path.join(os.path.dirname(__file__), "images"))
+    image = RandomResource(images)["image"]
+    return ViewImage(image, RandomPatchView(image, 200, 200), 200, 200)
+
+p = stream()
+
+for i in count(1):
+    image, = p.run(i)
+    cv2.imshow("Patch", cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    if cv2.waitKey() != 32:
+        break
+
